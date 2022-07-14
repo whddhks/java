f83922b3f517789fb5a91f0fc9f37fbd74f7ace5
jongwan <whddhks2204@naver.com>
2022-07-14 17:51:37 +0900

    java abstract/Interface 2

--- Begin/src/day09/MyClass2.java
+++ Begin/src/day09/MyClass2.java
@@ -7,7 +7,7 @@ interface Inter2{
 	String b();
 }
 interface HisInter extends Inter1, Inter2 {
-	
+	String STR="Hellp";
 }
 abstract class AbsClass {
 	abstract void c();
@@ -19,23 +19,36 @@ abstract class AbsClass {
  * */                                    
 
 
-public class MyClass2 extends AbsClass implements HisInter {
+public class MyClass2 extends AbsClass implements HisInter {//해본결과 extends가 먼저나와야함
+	
 	public void a() {System.out.println("MyClass's Inter1");}
+	
 	public String b() {return "hello~~";}
+	@Override
 	void c() {System.out.println("MyClass's AbsClass");}
 
 	public static void main(String[] args) {
-		/* 2]MyClass2타입의 객체 생성해서 메서드 각각 호출학소 STR값도 출력해보기
+		/* 2]MyClass2타입의 객체 생성해서 메서드 각각 호출하고 STR값도 출력해보기
 		 * 3] HisInter타입의 변수 선언하고 MyClass2객체생성해서 메소드 각각 호출하고 STR값도 출력해보기
 		 * 4] ABsClass타입의 변수 선언하고 MyClass2객체생성해서 메서드 각각 호출하고 STR값도 출력해보기
 		 * */
+		MyClass2 mc=new MyClass2();
+		mc.a();
+		mc.c();
+		System.out.println(mc.b());
+		System.out.println("-----------");
+		
 		AbsClass ac=new MyClass2();
+		ac.c();
+
+		((MyClass2)ac).a(); //가능
+		
 		
 		HisInter hi=new MyClass2();
-		ac.c();
 		hi.a();
 		System.out.println(hi.b());
-		
+		System.out.println(HisInter.STR);
+		System.out.println(MyClass2.STR);
 		
 		
 		
