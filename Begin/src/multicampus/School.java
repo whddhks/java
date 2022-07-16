d005c07b33a53b5d3769e1e3d8afcc08fa2fe8ed
jongwan <whddhks2204@naver.com>
2022-07-16 22:44:22 +0900

    java School program

--- Begin/src/multicampus/School.java
+++ Begin/src/multicampus/School.java
@@ -5,34 +5,60 @@ public class School {
 	//String[] arr=new String[3]; [x]
 	static Student[] arr=new Student[3];//배열 => DB같은 역할을 함
 	static int count=0;//배열의 인덱스 번호로 사용할 변수
+	static Teacher[] arr2=new Teacher[3];
+	static int count2=0;
+	
 	
 	public static void showMenu() {
 		System.out.println("**** SchoolApp v1.1******");
-		System.out.println("** 1. 등    록          **");
-		System.out.println("** 2. 출    력          **");
-		System.out.println("** 3. 검    색          **");
-		System.out.println("** 4. 삭    제          **");
-		System.out.println("** 9. 종    료          **");
+		System.out.println("**     1. 등    록      **");
+		System.out.println("**     2. 출    력      **");
+		System.out.println("**     3. 검    색      **");
+		System.out.println("**     4. 삭    제      **");
+		System.out.println("**     9. 종    료      **");
 		System.out.println("*************************");
 		System.out.println("메뉴 번호를 입력하세요 =>");
 		System.out.println("*************************");
 	}
 	
 	public static void register() {
-		Student s1=new Student();
-		s1.inputInfo();
-		System.out.println("--등록한 정보-----");
-		s1.showInfo();
-		
-		System.out.println("입력한 학생 정보를 저장할까요? [1. yes 2. no]");
 		Scanner sc=new Scanner(System.in);
+		System.out.println("1.학생 정보 등록 / 2.교사 정보 등록");
+		/*학생 교사 선택 12나누고
+		 * 정보 입력 및 등록1만 2개이쓰면 된다
+		 * */
 		int num=sc.nextInt();
-		if(num==1) {
-			//s1을 배열에 저장한다==> arr에 저장하기
-			arr[count]=s1;
-			count++;
-			System.out.println(count+"명 등록 했습니다!!");
+		if (num==1) {
+			Student s1=new Student();
+			s1.inputInfo();
+			System.out.println("-----등록한 학생 정보-----");
+			s1.showInfo();
+			System.out.println("입력한 학생 정보를 저장할까요? [1. yes 2. no]");
+			int num2=sc.nextInt();
+			if(num2==1) {
+				//s1을 배열에 저장한다==> arr에 저장하기
+				arr[count]=s1;
+				count++;
+				System.out.println(count+"명 등록 했습니다!!");
+			}
+		}else if (num==2) {
+			Teacher tc=new Teacher();
+			tc.inputInpo2();
+			System.out.println("-----등록한 교사 정보-----");
+			tc.showInfo2();
+			System.out.println("입력한 교사 정보를 저장할까요? [1. yes 2. no]");
+			int num3=sc.nextInt();
+			if (num3==1) {
+				arr2[count2]=tc;
+				count2++;
+				System.out.println(count2+"명 등록 완료하였습니다.");
+			}
 		}
+		
+		
+		
+		
+		
 	}//-------------------------------
 
 	public static void main(String[] args) {
--- Begin/src/multicampus/Teacher.java
+++ Begin/src/multicampus/Teacher.java
@@ -1,5 +1,5 @@
 package multicampus;
-
+import java.util.*;
 public class Teacher {
 	int no;//교번
 	String name;//이름
@@ -7,8 +7,25 @@ public class Teacher {
 	String phone;//연락처
 
 	//showInfo()
-	
+	public void showInfo2() {
+		System.out.println("교   번 : "+no);
+		System.out.println("이   름 : "+name);
+		System.out.println("담당 과목 : "+subject);
+		System.out.println("연 락 처 : "+phone);
+	}
 	//inputInfo()
+	public void inputInpo2() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("교번을 입력하십시오 : ");
+		no=sc.nextInt();
+		System.out.println("이름을 입력하십시오 : ");
+		name=sc.next();
+		System.out.println("담당교과을 입력하십시오 : ");
+		subject=sc.next();
+		System.out.println("연락처을 입력하십시오 : ");
+		phone=sc.next();
+	}
+			
 	
 	
 	
