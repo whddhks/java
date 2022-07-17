45a35396bf564394d177ca2d62bfc5d2710bf879
jongwan <whddhks2204@naver.com>
2022-07-17 20:47:50 +0900

    java School3

--- Begin/src/multicampus/School.java
+++ Begin/src/multicampus/School.java
@@ -8,7 +8,6 @@ public class School {
 	static Teacher[] arr2=new Teacher[3];
 	static int count2=0;
 	
-	
 	public static void showMenu() {
 		System.out.println("**** SchoolApp v1.1******");
 		System.out.println("**     1. 등    록      **");
@@ -28,33 +27,36 @@ public class School {
 		 * 정보 입력 및 등록1만 2개이쓰면 된다
 		 * */
 		int num=sc.nextInt();
-		if (num==1) {
-			Student s1=new Student();
-			s1.inputInfo();
-			System.out.println("-----등록한 학생 정보-----");
-			s1.showInfo();
-			System.out.println("입력한 학생 정보를 저장할까요? [1. yes 2. no]");
-			int num2=sc.nextInt();
-			if(num2==1) {
-				//s1을 배열에 저장한다==> arr에 저장하기
-				arr[count]=s1;
-				count++;
-				System.out.println(count+"명 등록 했습니다!!");
+			if (num==1) {
+				Student s1=new Student();
+				s1.inputInfo();
+				System.out.println("-----등록한 학생 정보-----");
+				s1.showInfo();
+				System.out.println("입력한 학생 정보를 저장할까요? [1. yes 2. no]");
+				int num2=sc.nextInt();
+				if(num2==1) {
+					//s1을 배열에 저장한다==> arr에 저장하기
+					arr[count]=s1;
+					count++;
+					System.out.println(count+"명 등록 했습니다!!");
+				}
+			}else if (num==2) {
+				Teacher tc=new Teacher();
+				tc.inputInpo2();
+				System.out.println("-----등록한 교사 정보-----");
+				tc.showInfo2();
+				System.out.println("입력한 교사 정보를 저장할까요? [1. yes 2. no]");
+				int num3=sc.nextInt();
+				if (num3==1) {
+					arr2[count2]=tc;
+					count2++;
+					System.out.println(count2+"명 등록 완료하였습니다.");
+				}
+				
 			}
-		}else if (num==2) {
-			Teacher tc=new Teacher();
-			tc.inputInpo2();
-			System.out.println("-----등록한 교사 정보-----");
-			tc.showInfo2();
-			System.out.println("입력한 교사 정보를 저장할까요? [1. yes 2. no]");
-			int num3=sc.nextInt();
-			if (num3==1) {
-				arr2[count2]=tc;
-				count2++;
-				System.out.println(count2+"명 등록 완료하였습니다.");
-			}
-		}
-	}//-------------------------------
+			
+	}
+		
 	public static void printAll() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("--모든 학생과 교사의 정보를 출력합니다.--");
@@ -62,11 +64,22 @@ public class School {
 		int num1=0;
 		num1=sc.nextInt();
 		if (num1==1) {
+			System.out.println("----학생 정보를 출력합니다.----");
 			for (int i=0;i<arr.length;i++) {
 				
+				Student st=arr[i];
+				st.showInfo();
+				System.out.println("-----------------");
 			}
-		}
+		}else if (num1==2) {
+			System.out.println("----교사 정보를 출력합니다.----");
+			for (int i=0;i<arr.length;i++) {
+				Teacher tc=arr2[i];
+				tc.showInfo2();
+				System.out.println("-----------------");
+			}
 		
+		}
 	}
 			
 	
@@ -89,6 +102,7 @@ public class School {
 				break;
 			case 2: //출력 printAll()==> arr에 저장된 학생들의 정보를 
 					//반복문 돌면서 출력한다
+				 School.printAll();
 				break;
 			case 3://검색
 				break;
